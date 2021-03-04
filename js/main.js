21fd4f0da572c6e27d2fe176bdc45cb2d98844ca
itacademyreact <itacademyreact@gmail.com>
2021-03-04 11:13:28 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// TODO LIST
+
+// 1. DEFINNIR. Qué queremos??
+//  Necesitamos una web con un input y un botón, que al introducir una tarea, se añada a una lista
+//  Proceso: usuario introduce valor -> añadimos elemento a la lista
+
+// 2. TAREAS
+//  - COMPLETAR HTML
+//  - MONTAR BASE JS
+//  - RELLENAR FUNCIONES
