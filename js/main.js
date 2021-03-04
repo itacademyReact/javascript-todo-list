85fc693c509b8a42619cbe5ecc872f754e828b00
itacademyreact <itacademyreact@gmail.com>
2021-03-04 11:51:09 +0100

    javascript logic

--- js/main.js
+++ js/main.js
@@ -2,9 +2,37 @@
 
 // 1. DEFINNIR. Qué queremos??
 //  Necesitamos una web con un input y un botón, que al introducir una tarea, se añada a una lista
-//  Proceso: usuario introduce valor -> añadimos elemento a la lista
+//  Proceso: usuario introduce valor-> pulsa boton -> añadimos elemento a la lista
 
 // 2. TAREAS
-//  - COMPLETAR HTML
-//  - MONTAR BASE JS
-//  - RELLENAR FUNCIONES
+//  -> COMPLETAR HTML
+//  -> MONTAR BASE JS
+//  -> RELLENAR FUNCIONES
+
+// VARIABLES
+// Con qué elementos del html voy a interaccionar?
+// Botón
+// Input
+// Listado para enganchar items -> ul
+var enterButton = document.getElementById("enter");
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul");
+
+// FUNCTIONS
+function addListAfterClick() {
+  if(input.value.length > 0) {
+    createListElement();
+  }
+}
+
+function createListElement() {
+  // Crear elemento, nuevo item
+  var li = document.createElement('li');
+  // Enganchamos el texto
+  li.appendChild(document.createTextNode(input.value));
+  // Añadimos el elemento en el ul
+  ul.appendChild(li);
+}
+
+// EVENTS
+enterButton.addEventListener("click", addListAfterClick);
